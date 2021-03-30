ae705ba61ce704b4c86a50093cc43bc69aa0e145
sagazirobiul <gazisagor00000@gmail.com>
2021-03-30 13:40:17 +0600

    ok

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ app.use(cors())
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uaoex.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
   const ordersCollection = client.db("emaJohnStore").collection("orders");
-
+  console.log(err);
   app.post('/addProduct', (req, res) => {
       const product = req.body;
       productsCollection.insertOne(product).then(result => {
@@ -24,7 +25,6 @@ client.connect(err => {
       productsCollection.find({})
       .toArray((err, documents) => {
         res.send(documents)
-        console.log(documents);
       })
   })
 
