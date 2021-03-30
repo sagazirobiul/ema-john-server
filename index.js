ba58ea6e5c1b24dd256890b57aa3dd435dcfb55f
sagazirobiul <gazisagor00000@gmail.com>
2021-03-30 16:00:27 +0600

    mongodb access issue fixed

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(cors())
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uaoex.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
@@ -45,7 +44,6 @@ client.connect(err => {
   app.post('/addOrder', (req, res) => {
     const order = req.body;
     ordersCollection.insertOne(order).then(result => {
-      console.log(result.insertedCount);
       res.send(result.insertedCount > 0)
     })
   })
